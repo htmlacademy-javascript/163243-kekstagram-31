21fab55e73b8d66e08cf9a778702083d81264edd
Dmitry Smirnov <coherence0@gmail.com>
2024-03-09 11:56:24 +0300

    Убрали ненужный код открытия тайтла

--- js/create-thumbs.js
+++ js/create-thumbs.js
@@ -4,19 +4,19 @@ const IMAGES_COUNT = 25;
 const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const imageContainer = document.querySelector('.pictures');
 
-// Удаляем скрытый класс
-imageContainer.querySelector('.pictures__title').classList.remove('visually-hidden');
+// Удаляем скрытый класс - оказалось тайтл здесь не нужен)
+// imageContainer.querySelector('.pictures__title').classList.remove('visually-hidden');
 
 const images = generatePhotos(IMAGES_COUNT);
 const imagesFragment = document.createDocumentFragment();
 
-images.forEach(({id, url, description, likes, comments}) => {
+images.forEach(({_, url, description, likes, comments}) => {
   const photoCard = imageTemplate.cloneNode(true);
   photoCard.querySelector('.picture__img')['src'] = url;
   photoCard.querySelector('.picture__img')['alt'] = description;
-  photoCard.querySelector('.picture__likes') = likes;
-  photoCard.querySelector('.picture__comments') = comments.length;
+  photoCard.querySelector('.picture__likes').textContent = likes;
+  photoCard.querySelector('.picture__comments').textContent = comments.length;
   imagesFragment.appendChild(photoCard);
 });
 
-imageContainer.appendChild(photoCard);
+imageContainer.appendChild(imagesFragment);
