762cc56f37861e270f9f1148a05f65ee0bc8ac96
Dmitry Smirnov <coherence0@gmail.com>
2024-03-10 12:32:40 +0300

    Перенесли вызов отрисовки превью картинок в маин

--- js/create-thumbs.js
+++ js/create-thumbs.js
@@ -1,22 +1,22 @@
-import { generatePhotos } from '../js/mocks/data.js';
+/**
+ * Функция для отрисовки превью картинок
+ * @param {array} images - принимает на вход сгенерированные (или полученные от сервера) картинки в виде массива объектов
+ */
+const createThumbs = (images) => {
+  const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const imageContainer = document.querySelector('.pictures');
+  const imagesFragment = document.createDocumentFragment();
 
-const IMAGES_COUNT = 25;
-const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const imageContainer = document.querySelector('.pictures');
+  images.forEach(({_, url, description, likes, comments}) => {
+    const photoCard = imageTemplate.cloneNode(true);
+    photoCard.querySelector('.picture__img')['src'] = url;
+    photoCard.querySelector('.picture__img')['alt'] = description;
+    photoCard.querySelector('.picture__likes').textContent = likes;
+    photoCard.querySelector('.picture__comments').textContent = comments.length;
+    imagesFragment.appendChild(photoCard);
+  });
 
-// Удаляем скрытый класс - оказалось тайтл здесь не нужен)
-// imageContainer.querySelector('.pictures__title').classList.remove('visually-hidden');
+  imageContainer.appendChild(imagesFragment);
+};
 
-const images = generatePhotos(IMAGES_COUNT);
-const imagesFragment = document.createDocumentFragment();
-
-images.forEach(({_, url, description, likes, comments}) => {
-  const photoCard = imageTemplate.cloneNode(true);
-  photoCard.querySelector('.picture__img')['src'] = url;
-  photoCard.querySelector('.picture__img')['alt'] = description;
-  photoCard.querySelector('.picture__likes').textContent = likes;
-  photoCard.querySelector('.picture__comments').textContent = comments.length;
-  imagesFragment.appendChild(photoCard);
-});
-
-imageContainer.appendChild(imagesFragment);
+export { createThumbs };
