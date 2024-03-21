23b46af77608ff615d53963a8edfb4aa438d833b
Dmitry Smirnov <coherence0@gmail.com>
2024-03-21 13:38:50 +0300

    Добавлена функция дозагрузки комментариев по клику

--- js/full-image.js
+++ js/full-image.js
@@ -1,11 +1,29 @@
-import { isEscapeKey, renderComment } from './util.js';
+import { isEscapeKey } from './util.js';
+
+const DEFAUT_COMMENTS_COUNT_SHOW = 5;
 
 const bigPictureElement = document.querySelector('.big-picture');
 const bigPicturePreviewElement = bigPictureElement.querySelector('.big-picture__preview');
 const bigPicturePhotoContainerElement = bigPicturePreviewElement.querySelector('.big-picture__img');
 const bigPictureImageElement = bigPicturePhotoContainerElement.querySelector('img');
 const bigPictureCloseElement = bigPictureElement.querySelector('.big-picture__cancel');
-const photoCardComments = bigPicturePreviewElement.querySelector('.social__comments');
+const bigPictureCommentsElement = bigPicturePreviewElement.querySelector('.social__comments');
+const commentsLoaderElement = bigPicturePreviewElement.querySelector('.comments-loader');
+const commentTemplateElement = bigPictureCommentsElement.lastElementChild.cloneNode(true);
+
+let comments = [];
+let currentCount = 0;
+
+
+/**
+ * Функция для очистки дефолтных комментариев
+ */
+function clearComments() {
+  while (bigPictureCommentsElement.lastElementChild) {
+    bigPictureCommentsElement.removeChild(bigPictureCommentsElement.lastElementChild);
+  }
+}
+
 
 /**
  * Функция закрытия просмотра большого изображения
@@ -13,6 +31,7 @@ const photoCardComments = bigPicturePreviewElement.querySelector('.social__comme
 const closeFullImage = () => {
   bigPictureCloseElement.parentElement.parentElement.classList.add('hidden');
   document.removeEventListener('keydown', documentKeydownHandler);
+  commentsLoaderElement.removeEventListener('click', commentsLoaderClickHandler);
   document.body.classList.remove('modal-open');
 };
 
@@ -29,49 +48,71 @@ function documentKeydownHandler(evt) {
 
 
 /**
- * Функция открытия просмотра большого изображения
+ * Функция генерации html кода одного комментария
+ * @param {str} avatar - пусть до аватарки автора
+ * @param {str} nickname - имя автора
+ * @param {str} commentText - текст комментация
+ * @returns {} - html код одного комментария
  */
-const openFullImage = () => {
+const renderComment = (avatar, nickname, commentText) => {
+  const comment = commentTemplateElement.cloneNode(true);
+  const commentAuthorAvatarElement = comment.querySelector('.social__picture');
+  const commentTextElement = comment.querySelector('.social__text');
+  commentAuthorAvatarElement.src = avatar;
+  commentAuthorAvatarElement.alt = nickname;
+  commentTextElement.textContent = commentText;
+  return comment;
+};
+
+
+/**
+ * Функция генерации html элемента блока комментариев
+ * @param {array} imageComments - массив с комментариями к конкретной фото
+ * @returns {HTMLElement} -  html элемент со списком комментариев
+ */
+const renderImageComments = () => {
+  clearComments();
+  const imageCommentsShow = comments.slice(0, currentCount + DEFAUT_COMMENTS_COUNT_SHOW);
+  imageCommentsShow.forEach(({avatar, message, name}) => bigPictureCommentsElement.appendChild(renderComment(avatar, name, message)));
+  commentsLoaderElement.addEventListener('click', commentsLoaderClickHandler);
+  bigPicturePreviewElement.querySelector('.social__comment-shown-count').textContent = imageCommentsShow.length;
+  if (imageCommentsShow.length >= comments.length) {
+    commentsLoaderElement.classList.add('hidden');
+  }
+};
+
+
+/**
+ * Функция открытия полного изображения
+ * @param {array} clickedImageDataComments - массив комментариев к конкретной фото
+ */
+const openFullImage = (clickedImageDataComments) => {
+  comments = clickedImageDataComments;
+  currentCount = 0;
   bigPictureElement.classList.remove('hidden');
   document.addEventListener('keydown', documentKeydownHandler);
   document.body.classList.add('modal-open');
-  bigPictureCloseElement.addEventListener('click', () => {
-    closeFullImage();
-  });
+  bigPictureCloseElement.addEventListener('click', closeFullImage);
+  renderImageComments();
 };
 
-/**
- * Функция генерации html кода комментариев
- * @param {array} imageComments - массив с комментариями к конкретной фото
- * @returns {string} - строка с html кодом комментариев
- */
-const renderImageComments = (imageComments) => {
-  let imageCommentsFeed = '';
-  imageComments.forEach(({avatar, message, name}) => {
-    imageCommentsFeed += renderComment(avatar, name, message);
-  });
-  return imageCommentsFeed;
-};
+function commentsLoaderClickHandler() {
+  currentCount += DEFAUT_COMMENTS_COUNT_SHOW;
+  renderImageComments();
+}
+
 
 /**
  * Функция отрисовки большого изображения фото
- * @param {obj} clickedImage - картинка, по которому кликнули
  * @param {array} clickedImageComments - массив комментариев к картинке
- * @returns {null}
  */
 const renderFullImage = (clickedImageData) => {
   bigPictureImageElement.src = clickedImageData.url;
   bigPicturePreviewElement.querySelector('.likes-count').textContent = clickedImageData.likes;
-  bigPicturePreviewElement.querySelector('.social__comment-shown-count').textContent = clickedImageData.comments.length;
   bigPicturePreviewElement.querySelector('.social__comment-total-count').textContent = clickedImageData.comments.length;
   bigPictureImageElement.alt = bigPicturePreviewElement.querySelector('.social__caption').textContent = clickedImageData.description;
-  // скрываем на потом
-  // bigPicturePreviewElement.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicturePreviewElement.querySelector('.comments-loader').classList.add('hidden');
-  photoCardComments.innerHTML = renderImageComments(clickedImageData.comments);
-  openFullImage(photoCardComments);
-
-  // console.log(renderImageComments(clickedImageComments));
+  commentsLoaderElement.classList.remove('hidden');
+  openFullImage(clickedImageData.comments);
 };
 
 
