76c36954d21af8a8ac2d84aac3400adb5548f9da
Keks <keks@htmlacademy.ru>
2024-03-24 21:35:45 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/render-thumbs.js
@@ -0,0 +1,25 @@
+/**
+ * Функция для отрисовки превью картинок
+ * @param {array} images - принимает на вход сгенерированные (или полученные от сервера) картинки в виде массива объектов
+ */
+const renderThumbs = (images) => {
+  const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
+  const imageContainer = document.querySelector('.pictures');
+  const imagesFragment = document.createDocumentFragment();
+
+  images.forEach(({url, description, likes, comments, id}) => {
+    const photoCard = imageTemplate.cloneNode(true);
+    const imageElement = photoCard.querySelector('.picture__img');
+    imageElement.src = url;
+    imageElement.alt = description;
+    imageElement.dataset.id = id;
+    photoCard.querySelector('.picture__likes').textContent = likes;
+    photoCard.querySelector('.picture__comments').textContent = comments.length;
+    imagesFragment.appendChild(photoCard);
+  });
+
+  imageContainer.appendChild(imagesFragment);
+  return imageContainer;
+};
+
+export { renderThumbs };
